6c592decca517bed5658f3463952b848b35a052f
Muhammad Adlan Jazuli <adlanjazuli@gmail.com>
2021-11-15 11:08:24 +0700

    update function detailpage in player controller

--- app/player/controller.js
+++ app/player/controller.js
@@ -23,23 +23,28 @@ module.exports = {
         }
     },
 
-    detailPage: async(req, res) => {
-        try {
-            const {id} = req.params
-            const voucher = await Voucher.findOne({ _id : id})
-                .populate('category')
-                .populate('nominals')
-                .populate('user', '_id name phoneNumber')    
-
-            if(!voucher){
-                return res.status(404).json({ message: 'Voucher game not found' })
-            }
-
-            res.status(200).json({ data: voucher })
-
-        } catch (err) {
-            res.status(500).json({message: err.message || `Internal server error`})
+    detailPage: async (req, res) => {
+      try {
+        const voucher = await Voucher.findOne({ _id: req.params.id })
+          .populate('user', '_id name phone_number')
+          .populate('category')
+          .populate('nominals')
+  
+        const payment = await Payment.find().populate('banks')
+  
+        if (!voucher) {
+          return res.status(404).json({ message: "voucher game tidak ditemukan.!" })
         }
+  
+        res.status(200).json({
+          data: {
+            detail: voucher,
+            payment
+          }
+        })
+      } catch (err) {
+        res.status(500).json({ message: err.message || "Internal server error" })
+      }
     },
 
     category: async(req, res) => {
